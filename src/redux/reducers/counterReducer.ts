d9ce74491ec4dc2a9ba7043ec2bf5061bc4dee89
Asifdvp <asif.mustafayev.13@gmail.con>
2022-07-31 11:35:38 +0400

    reducers

---
+++ src/redux/reducers/counterReducer.ts
@@ -0,0 +1,4 @@
+const init = 0
+export const profileReducer = (state=init,aciton:any) =>{
+    return state
+}
---
+++ src/redux/reducers/profileReducer.ts
@@ -0,0 +1,8 @@
+const init = {
+    name:"Asif",
+    age:"21",
+    jib:"developer"
+}
+export const profileReducer = (state=init,aciton:any) =>{
+    return state
+}
